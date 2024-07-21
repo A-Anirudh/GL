3724c4ece3fa63e3ece310526e06c6b95866f835
A-Anirudh <70565463+A-Anirudh@users.noreply.github.com>
2024-07-21 23:02:46 +0530

    Create splitTo4.py

---
+++ splitTo4.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('fruit.png')
+img_mat = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
+width, height, channel = img_mat.shape
+
+midx = width//2
+midy = height//2
+
+images = [image_mat[:midy,:midx], image_mat[:midy, midx:], iamge_mat[midy:, :midx], image_mat[midy:, midx:]]
+titles = ["top left","top right","bottom left","bottom right"]
+
+fig, axs = plt.subplot(2,2,figsize=(10,10))
+
+r = 0
+for i in range(2):
+  for j in range(2):
+    axs[i,j] = imshow(images[row+i+j])
+    axs[i,j].set_title(titles[row+i+j])
+    axs[i,j].show("off")
